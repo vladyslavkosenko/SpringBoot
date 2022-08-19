64302ea51046df759617c5cd2e8c9d0de405a346
vladyslavkosenko <vaduha91@gmail.com>
2022-08-19 17:04:02 +0300

    HOMEWORK_2

--- src/main/java/com/example/SpringBoot/controller/UUIDController.java
+++ src/main/java/com/example/SpringBoot/controller/UUIDController.java
@@ -1,5 +1,6 @@
 package com.example.SpringBoot.controller;
 
+import org.example.UUIDService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
